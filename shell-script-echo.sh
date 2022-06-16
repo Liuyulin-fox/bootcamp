2d9999bc390d72f8b3e43160937ba1e27cbd8907
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-06-16 17:24:05 +0900

    shell-script-echo.sh

--- shell-script-echo.sh
+++ shell-script-echo.sh
@@ -30,10 +30,10 @@ goods_dress_price=40000
 goods_discount=2
 
 #Sentence
-echo $number1_name 「性別: $number1_sex 、年齢: $number1_age 」は $shop に立ち寄って、$goods_coat を原価 $goods_coat_price を `expr $goods_coat_price / $goods_discount` に購入しました。 
-echo $number2_name 「性別: $number2_sex 、年齢: $number2_age 」は $shop に立ち寄って、$goods_shoes を原価 $goods_shoes_price を `expr $goods_shoes_price / $goods_discount` に購入しました。 
-echo $number3_name 「性別: $number3_sex 、年齢: $number3_age 」は $shop に立ち寄って、$goods_dress を原価 $goods_dress_price を `expr $goods_dress_price / $goods_discount` に購入しました。 
-echo $number4_name 「性別: $number4_sex 、年齢: $number4_age 」は $shop に立ち寄って、$goods_pants を原価 $goods_pants_price を `expr $goods_pants_price / $goods_discount` に購入しました。 
+echo $number1_name "「性別: $number1_sex 、年齢: $number1_age 」は $shop に立ち寄って、$goods_coat を原価 $goods_coat_price を `expr $goods_coat_price / $goods_discount` に購入しました。" 
+echo $number2_name "「性別: $number2_sex 、年齢: $number2_age 」は $shop に立ち寄って、$goods_shoes を原価 $goods_shoes_price を `expr $goods_shoes_price / $goods_discount` に購入しました。"
+echo $number3_name "「性別: $number3_sex 、年齢: $number3_age 」は $shop に立ち寄って、$goods_dress を原価 $goods_dress_price を `expr $goods_dress_price / $goods_discount` に購入しました。"
+echo $number4_name "「性別: $number4_sex 、年齢: $number4_age 」は $shop に立ち寄って、$goods_pants を原価 $goods_pants_price を `expr $goods_pants_price / $goods_discount` に購入しました。"
 
 
 #--------------------------------------------------------------------------------
