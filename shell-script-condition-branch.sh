84bb5dc95c3a22a270f4a203a9a11c0cde1111bb
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-06-16 18:54:02 +0900

    shell-script-condition-branch.sh

--- shell-script-condition-branch.sh
+++ shell-script-condition-branch.sh
@@ -6,64 +6,52 @@ echo 'ウリン大学の皆さん、ようこそ! 試験の評価の確認はこ
 read -p "成績を入力ください，成绩範囲0-100: " score
 
 if [ -z `echo $score | egrep '^[0-9]+$'` ];then
-
-    echo "入力されたグレードが正しい形式ではない"
-
+  echo "入力されたグレードが正しい形式ではない"
 fi
 
 if ((score >= 90));then
-
-    echo 'excellent'
-
+  echo 'excellent'
 elif ((score >= 80));then
-
-    echo 'good'
-
+  echo 'good'
 elif ((score >= 70));then
-
-    echo 'generally'
-
+  echo 'generally'
 elif ((score >= 60 ));then
-
-    echo 'pass'
-
+  echo 'pass'
 else
-
-    echo 'failed'
-
+  echo 'failed'
 fi
 
 #if文例①
 echo 'if文の"文字列"比較「空っぽ文字列」、引数値は""である'
 str1=""
 
-if [ -z $str1 ]
+if [ -z $str1 ] 
 then
-    echo "Empty string"
+  echo "Empty string"
 else
-    echo "Not empty string"
+  echo "Not empty string"
 fi
 
 #if文例②
 echo 'if文の"文字列"比較「同じ文字列」、引数値は"string"である'
 str2="string"
 
-if [ "string" == $str2 ]
+if [ "string" == $str2 ] 
 then
-    echo "Same strings"
+  echo "Same strings"
 else
-    echo "Different Strings"
+  echo "Different Strings"
 fi
 
 #if文例③
 echo 'if文の"文字列"比較「異なる文字列」引数値は"Not string"である'
-str3="Not string"
+str3="Not_string"
 
-if [ "string" == $str3 ]
+if [ "string" == $str3 ] 
 then
-    echo "Same strings"
+  echo "Same strings"
 else
-    echo "Different Strings"
+  echo "Different Strings"
 fi
 
 
