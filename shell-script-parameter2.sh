4a52274749f96f4d1af28b21a64f1faf2cb3454d
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-06-19 03:44:28 +0900

    shell-script-parameter1.sh

---
+++ shell-script-parameter2.sh
@@ -0,0 +1,15 @@
+#パラメータ（コマンドライン引数）を渡し、それらを出力(echo)する shell script をコードする②
+#パラメータの2個で2つファイル作成
+CMDNAME=`basename $0`
+if [ $# -ne 2 ]; then
+  echo "Usage: $CMDNAME file1 file2" 1>&2
+  exit 1
+fi
+
+date '+%D' >$1
+date '+%R' >$2
+
+echo "Check created files ..."
+ls -l $1 $2
+
+exit 0
