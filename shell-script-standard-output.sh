23bdddfabef17ac71f84e8c01bf7e561b48ec3ed
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-06-21 21:31:29 +0900

    shell-script-standard-output.sh

--- shell-script-standard-output.sh
+++ shell-script-standard-output.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-#コマンドの標準出力をfileに書き込む。キーボードから入力した結果をstandard-output.txtに出力する。
-$ echo 標準入力【standard input】stdin > standard-output.txt
+#標準出力(STDOUT)をファイルにリダイレクトする shell script をコードする
+echo 標準入力【standard input】stdin > standard-output.txt
 
 #コマンドの標準出力をfileに追加する。
-$ echo とは：プログラムに値を渡す入力元のことである。>> standard-output.txt
-$ echo 番号：0 >> standard-output.txt
-$ echo 装置：キーボード >> standard-output.txt
+echo とは：プログラムに値を渡す入力元のことである。>> standard-output.txt
+echo 番号：0 >> standard-output.txt
+echo 装置：キーボード >> standard-output.txt
 
-$ echo 標準出力【standard output】stdout >> standard-output.txt
-$ echo とは：プログラムから出力される値の出力先のことである。 >> standard-output.txt
-$ echo 番号：1 >>  standard-output.txt
-$ echo 装置：ディスプレイ >> standard-output.txt
-
-$ cat standard-output.txt
+echo 標準出力【standard output】stdout >> standard-output.txt
+echo とは：プログラムから出力される値の出力先のことである。 >> standard-output.txt
+echo 番号：1 >>  standard-output.txt
+echo 装置：ディスプレイ >> standard-output.txt
