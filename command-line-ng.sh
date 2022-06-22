ca46fa1d74f7d171f590fe10c31713a92832ee23
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-06-22 19:19:38 +0900

    shell-script-command-line-ng

--- command-line-ng.sh
+++ command-line-ng.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #コマンドライン引数に"ng"を渡すと標準エラー出力（エラーメッセージはなんでも良い）をng.txtへリダイレクト
 #command lineから引数「ng」を渡す
-NON_EXISTENT_FILE="$1"
-echo `env ${NON_EXISTENT_FILE} 2>ng.txt`
+
+if [ "$1" = "ng" ]; then 
+  NON_EXISTENT_ARGUMENT="$1";
+  env NON_EXISTENT_ARGUMENT;
+  echo 'コマンドライン引数は"ng"のみ標準エラー出力' >&2>ng.txt
+else
+  echo "コマンドライン引数はngではありません"
+fi 
+
+
+
