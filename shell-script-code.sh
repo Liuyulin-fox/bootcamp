b0ef0183d1ac0a8a04be2e9eeb0c98ffef151f55
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-06-24 13:19:36 +0900

    shell-script-code.sh

--- shell-script-code.sh
+++ shell-script-code.sh
@@ -8,13 +8,8 @@ MY_SCRIPT_PATH=$(cd $(dirname $0) && pwd)
 mkdir -p "${MY_SCRIPT_PATH}/out"
 
 #3. 渡された引数の回数分だけ"out"ディレクトリ以下にディレクトリを生成する
-command_line_argument_number_of_time=$1;
-for n1 in `seq 1 ${command_line_argument_number_of_time}`
-do
-  mkdir -p "${MY_SCRIPT_PATH}/out/dir-${n1}"
-done
-
 #4. 生成されたディレクトリに順序分（インデックス分）の数のファイルを生成する
+command_line_argument_number_of_time=$1;
 for n1 in `seq 1 ${command_line_argument_number_of_time}`
 do
   mkdir -p "${MY_SCRIPT_PATH}/out/dir-${n1}"
