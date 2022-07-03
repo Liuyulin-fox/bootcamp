35df7bcb9ed6243918a92a610eadad84badeabf5
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-07-03 23:11:10 +0900

    ruby-regexp

---
+++ ruby-regexp.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/ruby
+# 正規表現 とは何か、Regexpクラス とは何か、を調べ、その利用例をコードする
+
+# 正規表現とは、パターンを指定して、文字列を効率よく検索・置換するためのミニ言語
+# 利用例①
+text = <<-TEXT
+名前：YULIN LIU
+電話：07-1234-5678
+住所：東京都目黒区下目黒1-5-16
+TEXT
+
+puts text
+puts text.scan /\d\d-\d\d\d\d-\d\d\d\d/ # => ["07-1234-5678"]
+
+# 利用例②
+test_shigai = <<-TEXT
+名前：YULIN LIU
+電話：07-1234-5678
+電話：090-1234-5678
+電話：0875-12-3456
+電話：04797-1-2345
+住所：東京都目黒区下目黒1-5-16
+TEXT
+
+puts test_shigai
+puts test_shigai.scan /\d{2,5}-\d{1,4}-\d{4}/
+#07-1234-5678
+#090-1234-5678
+#0875-12-3456
+#04797-1-2345
+
+# 利用例③
+test_brackets = <<-TEXT
+名前：YULIN LIU
+電話：07(1234)5678
+電話：090-1234-5678
+電話：0875(12)3456
+電話：04797-1-2345
+住所：東京都目黒区下目黒1-5-16
+TEXT
+
+puts test_brackets
+puts test_brackets.scan /\d{2,5}[-(]\d{1,4}[-)]\d{4}/
+#07-1234-5678
+#090-1234-5678
+#0875-12-3456
+#04797-1-2345
+
+# regexpクラスとは、正規表現のクラス。
+# 利用例
+str = "this is the most wonderful world"
+rp1 = Regexp.new("^this is the most wonderful world")
+p rp1 =~ str           # => 0「合致」
+p Regexp.last_match[0] # => "this is the most wonderful world"
+
