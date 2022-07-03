6da9f3b64d4544fa20b749c45f5ec00c9dddaceb
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-07-04 01:08:04 +0900

    ruby-regexp

--- ruby-regexp.rb
+++ ruby-regexp.rb
@@ -46,6 +46,38 @@ puts test_brackets.scan /\d{2,5}[-(]\d{1,4}[-)]\d{4}/
 #0875-12-3456
 #04797-1-2345
 
+# 利用例④
+text = <<-TEXT
+クープバゲットのパンは美味しかった。
+今日はクープ バゲットさんに行きました。
+クープ　バゲットのパンは最高。
+ジャムおじさんのパン、ジャムが入ってた。
+また行きたいです。クープ・バゲット。
+クープ・バケットのパン、売り切れだった（><）
+TEXT
+
+puts text.split(/\n/).grep(/クープ.?バ[ゲケ]ット/)
+
+# 利用例⑤
+html = <<-HTML
+<select name="game_console">
+<option value="none"></option>
+<option value="wii_u" selected>Wii U</option>
+<option value="ps4">プレステ4</option>
+<option value="gb">ゲームボーイ</option>
+</select>
+HTML
+
+
+replaced = html.gsub(/<option value="(\w+)"(?: selected)?>(.*)<\/option>/, '\1,\2')
+puts replaced
+# <select name="game_console">
+# none,
+# wii_u,Wii U
+# ps4,プレステ4
+# gb,ゲームボーイ
+# </select>
+
 # regexpクラスとは、正規表現のクラス。
 # 利用例
 str = "this is the most wonderful world"
