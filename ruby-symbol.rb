45ed1d665842add83a723d3ddcd9a9ed8bd7af8d
Liuyulin-fox <brigitteliuyulin@gmail.com>
2022-07-03 15:12:56 +0900

    ruby-symbol.rb

--- ruby-symbol.rb
+++ ruby-symbol.rb
@@ -5,13 +5,13 @@
 # シンボルは文字列の皮を被った数値。
 
 # String
-ms_Liu = "Liu_String"
-mr_String = "Liu_String"
-puts ms_Liu.equal? mr_String #false
+ms_liu = "liu_string"
+mr_string = "liu_string"
+puts ms_liu.equal? mr_string #false
 
 # Symbol
 # シンボルが同じオブジェクトは、必ず同じオブジェクトになる。
 # シンボルの使い方「:」
-ms_Liu_l = :Liu_Symbol
-mr_Symbol = :Liu_Symbol
-puts ms_Liu_l.equal? mr_Symbol #true
+ms_liul = :liu_symbol
+mr_symbol = :liu_symbol
+puts ms_liul.equal? mr_symbol #true
